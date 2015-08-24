679dfdf53b4f3cf75b6d2c5ad02d703af24c83c6
Youngboom <youngboom.kim@gmail.com>
2015-08-24 20:52:50 +0900

    1. Add sample of setting file. 2. Add speaker App Store and play package file that constants are written.

---
+++ speaker/appstore/__init__.py
@@ -0,0 +1,30 @@
+APPSTORE = 'appstore'
+
+NAMESPACE = {'ns': 'http://www.apple.com/itms/'}
+
+REGIONS = {
+    'kr': {
+        'app_store_id': '143466-2',
+        'langs': ['ko', 'en']
+    },
+    'jp': {
+        'app_store_id': '143462-1',
+        'langs': ['ja', 'en']
+    },
+    'tw': {
+        'app_store_id': '143470-2',
+        'langs': ['zh', 'zh-tw', 'zh-cn', 'en']
+    },
+    'th': {
+        'app_store_id': '143475-2',
+        'langs': ['th', 'en']
+    },
+    'sg': {
+        'app_store_id': '143464-2',
+        'langs': ['en', 'ms', 'tm', 'zh', 'zh-tw']
+    },
+    'us': {
+        'app_store_id': '143441-1',
+        'langs': ['en', 'es', 'zh-tw', 'zh-cn', 'ko', 'ja', 'vn', 'th']
+    }
+}
---
+++ speaker/play/__init__.py
@@ -0,0 +1,19 @@
+GOOGLE_PLAY = 'google_play'
+REGIONS = {
+    'kr': {
+        'lang': 'ko',
+        'date_format': '%Y년 %m월 %d일'
+    },
+    'jp': {
+        'lang': 'ja',
+        'date_format': '%Y年%m月%d日'
+    },
+    'tw': {
+        'lang': 'zh_TW',
+        'date_format': '%Y年%m月%d日'
+    },
+    'th': {
+        'lang': 'th',
+        'date_format': '%d %m %Y'
+    }
+}
